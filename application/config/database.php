9a02d4b60785ccc6db651a66a855750ec4f0b8ac
yanda94 <yanda@jaringpangan.com>
2022-12-02 23:09:38 +0700

    update db

--- application/config/database.php
+++ application/config/database.php
@@ -75,9 +75,9 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
+	'hostname' => '103.161.185.21',
+	'username' => 'jaringpangan',
+	'password' => '@Japang2022',
 	'database' => 'rekam_medis',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
